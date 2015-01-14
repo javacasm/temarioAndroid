9dc4db991e69b72d459a3a8030e75c85908399be
javacasm <javacasm@gmail.com>
2015-01-14 12:54:36 +0100

    Añadido Listener de evento

--- app/src/main/java/com/cep/javacasm/calculadora233/MainActivity.java
+++ app/src/main/java/com/cep/javacasm/calculadora233/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,6 +16,11 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+    public void pulsacion(View v)
+    {
+        Toast.makeText(this, R.string.pulsacion_tecla, Toast.LENGTH_LONG).show();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
